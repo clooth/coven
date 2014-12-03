e8e856b3bb142d107ccabb30301fc71c7da67b2c
Nico Hämäläinen <nico@sizeof.io>
2014-12-03 15:56:45 +0200

    Initial Commit

---
+++ coven/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  coven
+//
+//  Created by Nico Hämäläinen on 03/12/14.
+//  Copyright (c) 2014 sizeof.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ coven/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  coven
+//
+//  Created by Nico Hämäläinen on 03/12/14.
+//  Copyright (c) 2014 sizeof.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
